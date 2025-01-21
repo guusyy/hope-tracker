cd67140978db37635acecbc63e99931e309da48a
Guus <guus.noij@gmail.com>
2025-01-21 21:06:51 +0100

    fix: change timestamp from utc to browser default

--- app/util/time.ts
+++ app/util/time.ts
@@ -1,8 +1,8 @@
 export const formatTimestamp = (timestamp: number): string => {
   const date = new Date(timestamp);
-  const hours = date.getUTCHours();
-  const minutes = date.getUTCMinutes();
-  const seconds = date.getUTCSeconds();
+  const hours = date.getHours(); // Use local time
+  const minutes = date.getMinutes(); // Use local time
+  const seconds = date.getSeconds(); // Use local time
 
   return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
     2,
