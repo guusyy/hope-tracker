fe944d6dc780953dc367a02b5c35b2b929382114
Guus <guus.noij@gmail.com>
2025-01-23 21:30:38 +0100

    feat: add spinner for more obvious fetching of new pricing after 10 seconds

--- app/[assetId]/components/asset-info.tsx
+++ app/[assetId]/components/asset-info.tsx
@@ -7,9 +7,12 @@ import { formatTimestamp } from "@/util/time";
 import { AssetLogo } from "@/app/components/asset-logo";
 import { AssetHistoryChart } from "./asset-history-chart";
 import { AssetPrice } from "@/app/components/asset-price";
+import { Spinner } from "@/components/ui/spinner";
+import { useLoaderWithMinimumTime } from "@/hooks/common";
 
 export function AssetInfo({ assetId }: { assetId: string }) {
-  const { data: assetData, isLoading } = useQuery(assetOptions(assetId));
+  const { data: assetData, isLoading, isFetching } = useQuery(assetOptions(assetId));
+  const loaderVisible = useLoaderWithMinimumTime(isFetching, 1000);
 
   if (isLoading) {
     return <div>Loading...</div>
@@ -26,7 +29,10 @@ export function AssetInfo({ assetId }: { assetId: string }) {
 
         <div>
           <h1 className="font-bold text-xl">{assetData?.data?.name}</h1>
-          <p><AssetPrice className="text-lg" asset={assetData.data} /></p>
+          <div className="flex gap-1">
+            <AssetPrice className="text-lg" asset={assetData.data} />
+            {loaderVisible && <Spinner size="small" />}
+          </div>
           <p className="italic text-gray-300 text-xs">Last price update from: {formatTimestamp(assetData.timestamp)}</p>
         </div>
       </div>
--- hooks/common.ts
+++ hooks/common.ts
@@ -15,3 +15,33 @@ export function useDebounce<T>(value: T, delay: number) {
 
   return debouncedValue;
 }
+
+export function useLoaderWithMinimumTime(
+  isFetching: boolean,
+  minimumTime: number
+) {
+  const [loaderVisible, setLoaderVisible] = useState(false);
+  const [fetchingStarted, setFetchingStarted] = useState(false);
+
+  useEffect(() => {
+    if (isFetching) {
+      setFetchingStarted(true);
+      setLoaderVisible(true);
+      const timer = setTimeout(() => {
+        setLoaderVisible(false);
+      }, minimumTime);
+
+      return () => clearTimeout(timer);
+    } else if (fetchingStarted) {
+      // If fetching is done before the minimum time, we wait until the timer
+      // completes before hiding the loader
+      const timer = setTimeout(() => {
+        setLoaderVisible(false);
+      }, minimumTime);
+
+      return () => clearTimeout(timer);
+    }
+  }, [isFetching, fetchingStarted, minimumTime]);
+
+  return loaderVisible;
+}
